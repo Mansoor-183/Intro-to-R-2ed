c867c65a91f5f7b6a78e1604a8593c28c9eb7693
Mansoor Mughal <manmughal183@gmail.com>
2022-02-15 23:00:34 -0500

    Added Spaces

--- Intro to R.R
+++ Intro to R.R
@@ -1,10 +1,11 @@
 # R script to accompany Intro to R
 #
 # Copy for Mansoor Mughal
+#
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
-# in your script by putting "#" at the beginning of the lines.
+# in your script by putting "#" at the beginning of a line.
 # 
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
